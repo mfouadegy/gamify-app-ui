8c9ed0a0901da0294e49cafa26acfadd17a249cf
fouad <elfouadsoft@.gmail.com>
2020-11-11 11:13:17 +0200

    add featured Games and background gradient

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:app/data.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -6,10 +7,63 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Size size;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    size = MediaQuery
+        .of(context)
+        .size;
     return Scaffold(
-      body: Container(),
+      body: Stack(children: [
+        _featuredGames(),
+        _backgroundBox(),
+
+      ],),
+    );
+  }
+
+  Widget _featuredGames() {
+    return SizedBox(
+      height: size.height * .50,
+      width: size.width,
+      child: PageView(
+        scrollDirection: Axis.horizontal,
+        children: featuredGames.map((game) {
+          return Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(game.coverImage.url), fit: BoxFit.cover,)
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  Widget _backgroundBox(){
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        width: size.width,
+        height: size.height * .80,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(35, 45, 59, 1.0),
+              Colors.transparent
+            ],
+            stops: [.60,1.0],
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter
+          )
+        ),
+      ),
     );
   }
 }
